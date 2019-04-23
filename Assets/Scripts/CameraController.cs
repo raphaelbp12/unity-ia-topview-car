c00676830ce2370f06776a382174aeb951ee67ac
Raphael Parreira <raphaelbp12@gmail.com>
2019-04-22 22:06:35 -0300

    car imported

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject car;
+
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - car.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = car.transform.position + offset;
+    }
+}
