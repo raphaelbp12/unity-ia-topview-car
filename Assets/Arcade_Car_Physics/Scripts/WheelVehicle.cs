6a46df09913d7576cffefb4df9f9dc0f6d70b0f2
Raphael Parreira <raphaelbp12@gmail.com>
2019-04-25 00:24:40 -0300

    raycast ignoring goals

--- Assets/Arcade_Car_Physics/Scripts/WheelVehicle.cs
+++ Assets/Arcade_Car_Physics/Scripts/WheelVehicle.cs
@@ -289,11 +289,10 @@ namespace VehicleBehaviour {
         float GetLaserDistToWall(float maxDist, Vector3 direction)
         {
             // Bit shift the index of the layer (8) to get a bit mask
-            int layerMask = 1 << 8;
+            int layerMask = ~ ((1 << 8) | (1 << 9));
 
             // This would cast rays only against colliders in layer 8.
             // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-            layerMask = ~layerMask;
 
             RaycastHit hit;
             // Does the ray intersect any objects excluding the player layer
