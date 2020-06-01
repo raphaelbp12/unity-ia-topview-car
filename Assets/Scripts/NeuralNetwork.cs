9305a1fffb8e248e3518dea23e09186b60f52c46
Raphael Parreira <raphaelbp12@gmail.com>
2020-06-01 19:48:09 -0300

    moving walls irgnored by laser;

--- Assets/Scripts/NeuralNetwork.cs
+++ Assets/Scripts/NeuralNetwork.cs
@@ -200,7 +200,7 @@ public class NeuralNetwork : MonoBehaviour
     float GetLaserDistToWall(float maxDist, Vector3 direction)
     {
         // Bit shift the index of the layer (8) to get a bit mask
-        int layerMask = ~ ((1 << 8) | (1 << 9));
+        int layerMask = ~ ((1 << 8) | (1 << 9) | (1 << 10));
 
         // This would cast rays only against colliders in layer 8.
         // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
