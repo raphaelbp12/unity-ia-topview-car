80c2cbbd83b4cb981884a09bc0fb0873dd457c53
Raphael Parreira <raphaelbp12@gmail.com>
2021-05-22 23:38:13 -0300

    feat: :sparkles: add new kinematics saved cars

--- Assets/Scripts/GameRulesController.cs
+++ Assets/Scripts/GameRulesController.cs
@@ -64,7 +64,9 @@ public class GameRulesController : MonoBehaviour
     private string pathToTheFile = "./cars/";
     public InputField carFileName;
     //private List<string> existingCars = new List<string>();
-    private List<string> existingCars = new List<string>() { "massa", "dia1", "dia2", "dia3", "diferente", "diferente2", "azulao" };
+    // private List<string> existingCars = new List<string>() { "massa", "dia1", "dia2", "dia3", "diferente", "diferente2", "azulao" };
+    
+    private List<string> existingCars = new List<string>() { "terraria", "terraria1", "terraria2" };
 
     public int numberPersistentCars = 4;
 
