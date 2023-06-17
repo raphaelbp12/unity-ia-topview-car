1d0c28374c456c7475611e473c31f719b8a1b134
Raphael Parreira <raphaelbp12@gmail.com>
2023-06-17 18:25:35 +0200

    feat: run the game without the graphs

---
+++ Assets/Scripts/CommandLineRunner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class CommandLineRunner : MonoBehaviour
+{
+    public static void StartGame()
+    {
+        // Set the desired time scale (1 for normal speed)
+        Time.timeScale = 1;
+
+        // Load the desired scene by name or index
+        EditorSceneManager.OpenScene("Assets/Scenes/SampleScene.unity"); // TODO replace with desired start scene
+        EditorApplication.EnterPlaymode();
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # unity-ia-topview-car
 
 Unity LTS 2019.4.40f1
+
+# Run in No Graph Mode
+
+Go to your `Unity.exe` path and run the following command:
+
+`./Unity.exe -batchmode -nographics -projectPath "C:\code\Personal\unity-ia-topview-car" -executeMethod CommandLineRunner.StartGame`
